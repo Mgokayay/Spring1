e8f473a5dcf684eed453602a5824e3a9007ed387
Mgokayay <aymustafagokay@gmail.com>
2024-04-08 17:56:31 +0300

    entity created

---
+++ src/main/java/com/workintech/Spring1/entity/Animal.java
@@ -0,0 +1,27 @@
+package com.workintech.Spring1.entity;
+
+public class Animal {
+    private int id;
+    private String name;
+
+    public Animal(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
